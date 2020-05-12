c4c41eac0abd3bdf74092ade37146462dfe5272b
Jack Cullen <jpcullen20@gmail.com>
2020-05-12 15:19:02 -0600

    Add flash message

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,13 @@ class ReviewsController < ApplicationController
 
   def create
     shelter = Shelter.find(params[:shelter_id])
-    shelter.reviews.create!(review_params)
-    redirect_to "/shelters/#{shelter.id}"
+    review = shelter.reviews.new(review_params)
+    if review.save
+      redirect_to "/shelters/#{shelter.id}"
+    else
+      flash[:notice] = "Please fill out required fields."
+      redirect_to "/shelters/#{shelter.id}/reviews/new"
+    end
   end
 
   private
