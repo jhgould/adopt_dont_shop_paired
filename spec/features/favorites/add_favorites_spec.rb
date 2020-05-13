34baad8a4d25aaf4210a3c6cf56ff7e42b486717
Jack Cullen <jpcullen20@gmail.com>
2020-05-13 17:02:51 -0600

    Add pet creation spec

---
+++ spec/features/favorites/add_favorites_spec.rb
@@ -0,0 +1,57 @@
+require './spec/rails_helper'
+
+RSpec.describe "favorites", type: :feature do
+
+  before :each do
+    @shelter = Shelter.create(name: "Angels With Paws")
+    @pet1 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+      name: "Bruno",
+      approximate_age: "4",
+      sex: "M",
+      adoption_status: "Adoptable",)
+    @pet2 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Woody",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: "Adoptable",)
+    @pet3 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Jim",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: false,)
+    @pet4 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Koa",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: "Adoptable",)
+  end
+
+  describe "favorites indicator" do
+    it "shows number of favorites on the nav bar" do
+      visit "/shelters"
+      within "nav" do
+        expect(page).to have_content("Total Favorites: 0")
+      end
+
+      visit "/pets"
+      within "nav" do
+        expect(page).to have_content("Total Favorites: 0")
+      end
+    end
+  end
+
+  describe "favorite creation" do
+    it "" do
+      visit "/pets/#{@pet1.id}"
+      click_link "Favorite Pet"
+      expect(current_path).to eq("/pets/#{@pet1.id}")
+      expect(page).to have_content("#{@pet1.name} added to favorites.")
+      expect(page).to have_content("Total Favorites: 1")
+    end
+  end
+
+end
