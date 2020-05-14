cfe17c9c556278ab68c7e0a234a8d28dac4f8f46
Jack Cullen <jpcullen20@gmail.com>
2020-05-13 18:47:22 -0600

    Add favorite list model and controller to store and control favorites

--- app/controllers/favorite_controller.rb
+++ app/controllers/favorite_controller.rb
@@ -1,10 +1,8 @@
 class FavoriteController < ApplicationController
   def update
     pet = Pet.find(params[:id])
-    pet_id_str = pet.id.to_s
-    session[:favorites] ||= Hash.new(0)
-    session[:favorites][pet_id_str] ||= 0
-    session[:favorites][pet_id_str] = session[:favorites][pet_id_str] + 1
+    session[:favorites] ||= []
+    session[:favorites] << [pet]
     flash[:notice] = "#{pet.name} added to favorites."
     redirect_to "/pets/#{params[:id]}"
   end
