ddc986a486f1b6fc08d526c8685194a400a69f5c
Jack Gould <johnhilsmangould@gmail.com>
2020-05-16 17:35:26 -0600

    add test for user story 20 - applications index page

---
+++ spec/features/applications/application_index_spec.rb
@@ -0,0 +1,56 @@
+require './spec/rails_helper'
+
+RSpec.describe "applications index page", type: :feature do
+  before :each do
+    @shelter = Shelter.create(name: "Angels With Paws")
+    @pet1 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+      name: "Bruno",
+      approximate_age: "4",
+      sex: "M",
+      adoption_status: "Adoptable",)
+    @pet2 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Woody",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: "Adoptable",)
+    @pet3 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Jim",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: false,)
+    @pet4 = @shelter.pets.create!(
+      image_path: "https://cdn.pixabay.com/photo/2015/11/17/13/13/dogue-de-bordeaux-1047521_1280.jpg",
+      name: "Koa",
+      approximate_age: "2",
+      sex: "F",
+      adoption_status: "Adoptable",)
+    @application1 = create(:application)
+    @application2 = create(:application)
+    @application3 = create(:application)
+    PetApplication.create(application_id: @application1.id, pet_id: @pet1.id)
+    PetApplication.create(application_id: @application2.id, pet_id: @pet1.id)
+    PetApplication.create(application_id: @application3.id, pet_id: @pet2.id)
+  end
+
+  it "a user can see all the applicatons for a pet from the pets show page" do
+    visit "pets/#{@pet1.id}"
+
+    click_link "View All Applications"
+
+    expect(page).to have_content(@application1.name)
+    expect(page).to have_content(@application2.name)
+    expect(page).to_not have_content(@application3.name)
+
+    click_link "#{@application1.name}"
+    expect(current_path).to eq("/applicatons/#{@application1.id}")
+    click_link "#{@application2.name}"
+    expect(current_path).to eq("/applicatons/#{@application2.id}")
+    click_link "#{@application3.name}"
+    expect(current_path).to eq("/applicatons/#{@application3.id}")
+  end
+
+
+end
