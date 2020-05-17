42df7798a66a488acbd563f278321c182c7822f8
Jack Gould <johnhilsmangould@gmail.com>
2020-05-17 10:43:41 -0600

    add a new colum to applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200516160259) do
+ActiveRecord::Schema.define(version: 20200517160228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20200516160259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "zip"
+    t.boolean "approved"
   end
 
   create_table "pet_applications", force: :cascade do |t|
