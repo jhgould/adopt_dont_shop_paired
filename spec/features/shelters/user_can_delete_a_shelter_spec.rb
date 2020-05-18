b205fa428d3c48c09b0a5f093c2c4ea3cac916db
Jack Cullen <jpcullen20@gmail.com>
2020-05-18 15:45:34 -0600

    Test that non-pending pet and review gets deleted when shelter gets deleted

--- spec/features/shelters/user_can_delete_a_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_a_shelter_spec.rb
@@ -5,14 +5,24 @@ RSpec.describe "shelter delete", type: :feature do
   before(:each) do
     @shelter1 = Shelter.create(name: "Angels With Paws", address: "2540 Youngfield St", city: "Lakewood", state: "CO", zip: "80215")
     @shelter2 = Shelter.create(name: "MaxFund Dog Shelter", address: "1005 Galapago", city: "Denver", state: "CO", zip: "80204")
-    @pet = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+    @pet1 = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+                      name: "Bucky",
+                      description: "Best dog ever.",
+                      approximate_age: "4",
+                      sex: "M",
+                      shelter_id: @shelter1.id,)
+    @pet2 = Pet.create(image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
                       name: "Bruno",
                       description: "Best dog ever.",
                       approximate_age: "4",
                       sex: "M",
                       shelter_id: @shelter2.id,)
+    @review =  @shelter1.reviews.create!(
+                                    title: "Great",
+                                    rating: 5,
+                                    content: "This place was awesome")
     @application = create(:application)
-    PetApplication.create(application_id: @application.id, pet_id: @pet.id)
+    PetApplication.create(application_id: @application.id, pet_id: @pet2.id)
   end
 
   it "deletes the shelter when user clicks delete" do
@@ -32,7 +42,7 @@ RSpec.describe "shelter delete", type: :feature do
   it "Shelters with Pets that have pending status cannot be Deleted" do
     visit "/applications/#{@application.id}"
 
-    within ".pet-#{@pet.id}" do
+    within ".pet-#{@pet2.id}" do
       click_link "Approve Application"
     end
 
@@ -45,6 +55,13 @@ RSpec.describe "shelter delete", type: :feature do
     click_on "Delete Shelter"
     expect(page).to have_content("Shelters with pending pets cannot be deleted.")
     expect(page).to have_content(@shelter2.name)
+  end
+
+  it "non pending pets and review deleted when shelter is deleted" do
+    visit "/shelters"
+    click_on "delete-shelter-#{@shelter1.id}"
+    visit("/pets")
+    expect(page).to_not have_content("#{@pet1.name}")
   end
 
 end
