8c6ef5dbb4e9409ed121a7f3c23ee542afd74449
Jack Cullen <jpcullen20@gmail.com>
2020-05-19 14:49:32 -0600

    Add pet methodn to check whether any pets have approved apps

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,6 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Pet do
+
+  before :each do
+    @shelter = create(:shelter)
+    @pet1 = @shelter.pets.create(
+      image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+      name: "Bruno",
+      approximate_age: "4",
+      sex: "M",)
+    @pet2 = @shelter.pets.create(
+      image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
+      name: "Bruno",
+      approximate_age: "4",
+      sex: "M",)
+    @application = create(:application)
+    PetApplication.create(application_id: @application.id, pet_id: @pet1.id, approved: true)
+  end
+
   describe 'relationships' do
     it {should belong_to :shelter}
     it {should have_many :pet_applications}
@@ -13,20 +30,11 @@ RSpec.describe Pet do
 
   describe 'methods' do
     it "all_with_application" do
-      shelter = create(:shelter)
-      pet1 = shelter.pets.create(
-        image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
-        name: "Bruno",
-        approximate_age: "4",
-        sex: "M",)
-      pet2 = shelter.pets.create(
-        image_path: "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_1280.jpg",
-        name: "Bruno",
-        approximate_age: "4",
-        sex: "M",)
-      application = create(:application)
-      PetApplication.create(application_id: application.id, pet_id: pet1.id)
-      expect(Pet.all_with_application).to eq([pet1])
+      expect(Pet.all_with_application).to eq([@pet1])
+    end
+
+    it "has_approved_pets" do
+      expect(Pet.has_approved_pets).to eq(true)
     end
   end
 end
