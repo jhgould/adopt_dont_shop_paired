ad1699381103a39828c133cdc7068d73e776f8b0
Jack Cullen <jpcullen20@gmail.com>
2020-05-19 14:11:42 -0600

    Add spec for deleting a pet with a non-pending app

--- spec/features/pets/user_can_delete_a_pet_spec.rb
+++ spec/features/pets/user_can_delete_a_pet_spec.rb
@@ -40,6 +40,15 @@ RSpec.describe "pet delete", type: :feature do
     expect(page).to_not have_content(@pet.name)
   end
 
+  it "user can delete a pet with a non-pending application" do
+    application = create(:application)
+    PetApplication.create(application_id: application.id, pet_id: @pet.id)
+    visit "/pets"
+    click_link "delete-pet-#{@pet.id}"
+    expect(current_path).to eq("/pets")
+    expect(page).to_not have_content(@pet.name)
+  end
+
   it "user cannot delete a pet if it has a pending/approved application" do
     application = create(:application)
     PetApplication.create(application_id: application.id, pet_id: @pet.id)
@@ -63,7 +72,6 @@ RSpec.describe "pet delete", type: :feature do
     click_link "delete-pet-#{@pet.id}"
     expect(page).to have_content("Pets with approved/pending applications cannot be deleted")
     expect(page).to have_content(@pet.name)
-
   end
 
 end
