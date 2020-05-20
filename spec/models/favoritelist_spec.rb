44b295c59b102726bf68a34b0e1931ecac52614f
Jack Cullen <jpcullen20@gmail.com>
2020-05-20 16:07:09 -0600

    Move logic for checking if a pet is in favorites to the favorites model

--- spec/models/favoritelist_spec.rb
+++ spec/models/favoritelist_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Favoriteslist do
       approximate_age: "4",
       sex: "M",
       adoption_status: "Adoptable",)
-    @favorites = Favoriteslist.new([@pet1])
+    @favorites = Favoriteslist.new([@pet1.id.to_s])
   end
 
   describe "methods" do
@@ -25,27 +25,32 @@ RSpec.describe Favoriteslist do
     end
 
     it "add_favorite" do
-      @favorites.add_favorite(@pet2)
+      @favorites.add_favorite(@pet2.id.to_s)
       expect(@favorites.total_count).to eq(2)
     end
 
     it "remove_from_favorites" do
-      @favorites.add_favorite(@pet2)
-      @favorites.remove_from_favorites(@pet2)
+      @favorites.add_favorite(@pet2.id.to_s)
+      @favorites.remove_from_favorites(@pet2.id.to_s)
       expect(@favorites.total_count).to eq(1)
-      @favorites.remove_from_favorites(@pet1)
+      @favorites.remove_from_favorites(@pet1.id.to_s)
       expect(@favorites.total_count).to eq(0)
     end
 
     it "remove_all_from_favorites" do
-      @favorites.add_favorite(@pet2)
-      @favorites.remove_all_from_favorites([@pet1, @pet2])
+      @favorites.add_favorite(@pet2.id.to_s)
+      @favorites.remove_all_from_favorites([@pet1.id.to_s, @pet2.id.to_s])
       expect(@favorites.total_count).to eq(0)
     end
 
     it "clear_all" do
       @favorites.clear_all
       expect(@favorites.total_count).to eq(0)
+    end
+
+    it "has_pet" do
+      expect(@favorites.has_pet?(@pet1)).to eq(true)
+      expect(@favorites.has_pet?(@pet2)).to eq(false)
     end
   end
 end
